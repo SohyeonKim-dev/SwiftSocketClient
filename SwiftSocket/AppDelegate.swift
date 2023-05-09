6484cf79a1523ed6786aeb2f9ee06490685739c4
SohyeonKim-dev <happythgus89@gmail.com>
2023-05-09 18:02:05 +0900

    SwiftP

---
+++ SwiftSocket/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SwiftSocket
+//
+//  Created by 김소현 on 2023/05/09.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        
+    }
+}
---
+++ SwiftSocket/UIViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController.swift
+//  SwiftSocket
+//
+//  Created by 김소현 on 2023/05/09.
+//
+
+import UIKit
+
