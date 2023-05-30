a48534d356e6837924e8a64c19c9c26a31c9e76e
SohyeonKim-dev <happythgus89@gmail.com>
2023-05-30 23:41:00 +0900

    [FEAT] UDPClient

--- SwiftSocket/SocketManger/UDPEchoClient.swift
+++ SwiftSocket/SocketManger/UDPEchoClient.swift
@@ -8,14 +8,37 @@
 import Foundation
 import NIO
 
-let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-let bootstrap = DatagramBootstrap(group: group)
-    .channelInitializer { channel in
-        return channel.eventLoop.makeSucceededFuture(())
-    }
-    .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-    .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEPORT), value: 1)
-
-class UDPEchoClientManager: NSObject {
+class UDPEchoHandler: ChannelInboundHandler  {
+    typealias InboundIn = ByteBuffer
+    typealias OutboundOut = ByteBuffer
     
+    public func channelActive(context: ChannelHandlerContext) {
+        // TODO: Fix
+    }
+    
+    public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+//        let envelope = self.unwrapInboundIn(data)
+//        let byteBuffer = envelope
+    }
 }
+
+class UDPClient {
+    private let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+    private var host: String?
+    private var port: Int?
+    
+    init(host: String, port: Int) {
+        self.host = host
+        self.port = port
+    }
+    
+    private var clientBootstrap: DatagramBootstrap {
+        return DatagramBootstrap(group: group)
+            .channelInitializer { channel in
+                channel.eventLoop.makeSucceededFuture(())
+            }
+            .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+            .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEPORT), value: 1)
+    }
+}
+
